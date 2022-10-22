2232e6b8b806be486529d2a064becb9ecd388314
Diego Gonzalez <diegom.develops@hotmail.com>
2022-10-22 19:05:23 -0400

    feat: route
    Setting paths in an array

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,24 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from "vue-router";
 
+const appRoutes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "home",
+    component: () => import("../views/HomeView.vue"),
+  },
+  {
+    path: "/test",
+    name: "test",
+    component: () => import("../views/TestView.vue"),
+  },
+];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
-      path: "/test",
-      name: "test",
-      component: () => import("../views/TestView.vue"),
-    },
-  ],
+  routes: [...appRoutes],
 });
 
 export default router;
