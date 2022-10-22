a6c457a3c38cd4d686189496ff9ce884b0e1bed7
Diego González <diegom.develops@gmail.com>
2022-10-21 21:13:05 -0400

    feat: ThingForm
    Adding ThingForm component to registrate entity names

---
+++ src/components/ThingForm.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const inputs = ref([
+  {
+    name: "name",
+    type: "text",
+    placeHolder: "nombre",
+  },
+]);
+</script>
+
+<template>
+  <form class="thing-form">
+    <Input
+      v-for="input in inputs"
+      :input-name="input.name"
+      :input-type="input.type"
+      :input-placeholder="input.placeHolder"
+      :key="input.name"
+    />
+  </form>
+</template>
