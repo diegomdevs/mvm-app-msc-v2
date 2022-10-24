6c870ae00e51afa28a4c3354442f8b90c3a0997d
Diego Gonzalez <diegom.develops@hotmail.com>
2022-10-24 05:12:22 -0400

    feat: port
    heroku port

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: Number(process.env.PORT) || 3000,
+    port: Number(process.env.PORT),
   },
   resolve: {
     alias: {
